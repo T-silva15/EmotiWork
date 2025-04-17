89295b643b6491bcdd2fe504fdf5257bd591b801
Tiago Silva <tiagoissilva@hotmail.com>
2025-04-17 08:08:05 +0100

    profile page

---
+++ Pages/Profile.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmotiWork.Pages
+{
+	public class ProfileModel : PageModel
+	{
+		public void OnGet()
+		{
+
+		}
+	}
+}
