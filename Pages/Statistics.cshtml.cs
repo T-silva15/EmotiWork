87a55db7469e9e975ad8f21141b686b7696b2f74
Tiago Silva <tiagoissilva@hotmail.com>
2025-04-18 15:19:37 +0100

    dashboard and statistics pages

--- Pages/Statistics.cshtml.cs
+++ Pages/Statistics.cshtml.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace EmotiWork.Pages
 {
-    public class NotificationListModel : PageModel
+    public class StatisticsModel : PageModel
     {
         public void OnGet()
         {
+            // This is a prototype with visual elements only
+            // No data processing needed at this stage
         }
     }
 }
