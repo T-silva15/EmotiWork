9d8cbb12905a958b31eff378565802922e96326b
Tiago Silva <tiagoissilva@hotmail.com>
2025-04-19 22:07:13 +0100

    Report Checking Page

---
+++ Pages/Backoffice/Reports.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmotiWork.Pages.Backoffice
+{
+    public class ReportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
