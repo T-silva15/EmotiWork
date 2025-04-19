ff86c98b1d309614bf4908513d1bb17820c0f14c
Tiago Silva <tiagoissilva@hotmail.com>
2025-04-19 22:24:29 +0100

    Admin Statistics Page

---
+++ Pages/Backoffice/AdminStatistics.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmotiWork.Pages.Backoffice
+{
+    public class AdminStatisticsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
