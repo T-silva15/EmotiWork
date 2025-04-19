f41d16f4a5f752a7918c5af261ac487aa9c98f36
Tiago Silva <tiagoissilva@hotmail.com>
2025-04-19 16:29:55 +0100

    team management

---
+++ Pages/Backoffice/Teams.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmotiWork.Pages.Backoffice
+{
+    public class TeamsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
