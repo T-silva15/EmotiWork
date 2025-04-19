859392a225bd08d6a44de32d1e558060f301b74d
Tiago Silva <tiagoissilva@hotmail.com>
2025-04-19 10:45:48 +0100

    Reports screen

---
+++ Pages/Reports.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmotiWork.Pages
+{
+    public class ReportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
