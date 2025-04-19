1d3a5a85a54dbf4a7a91c65f64858aa6186803f9
Tiago Silva <tiagoissilva@hotmail.com>
2025-04-19 10:22:11 +0100

    Questionnaire Page

---
+++ Pages/Questionnaire.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmotiWork.Pages
+{
+    public class QuestionnaireModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
