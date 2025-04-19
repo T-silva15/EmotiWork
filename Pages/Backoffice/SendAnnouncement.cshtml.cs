7cb792f9158d10c9f3b7f559446c18b21c6f4c1f
Tiago Silva <tiagoissilva@hotmail.com>
2025-04-19 23:12:47 +0100

    send Announcement page

---
+++ Pages/Backoffice/SendAnnouncement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmotiWork.Pages.Backoffice
+{
+    public class SendAnnouncementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
