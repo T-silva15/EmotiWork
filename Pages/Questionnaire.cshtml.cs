5ac1c54c43d5a006f3a003d738f9f6726e97bdf3
Tiago Silva <tiagoissilva@hotmail.com>
2025-04-30 11:35:51 +0100

    user journey improvements

--- Pages/Questionnaire.cshtml.cs
+++ Pages/Questionnaire.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
 
 namespace EmotiWork.Pages
 {
-    public class QuestionnaireModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class QuestionnaireModel : PageModel
+	{
+		public bool IsQuestionnaireSubmitted { get; set; }
+
+		[BindProperty]
+		public string[] Answers { get; set; } = new string[5];
+
+		public void OnGet()
+		{
+			IsQuestionnaireSubmitted = HttpContext.Session.GetString("QuestionnaireSubmitted") == "true";
+		}
+
+		public IActionResult OnPost()
+		{
+			HttpContext.Session.SetString("QuestionnaireSubmitted", "true");
+			IsQuestionnaireSubmitted = true;
+
+			return RedirectToPage("/Home");
+		}
+	}
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+	options.IdleTimeout = TimeSpan.FromHours(24);
+	options.Cookie.HttpOnly = true;
+	options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
@@ -12,6 +18,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseSession();
 
 app.UseHttpsRedirection();
 
